9f9d06c4a39f22df171d19368c8ed9caa67f16d9
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2023-11-10 09:27:05 +0700

    Initial Commit

---
+++ CardJson/CardJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardJsonApp.swift
+//  CardJson
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CardJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
